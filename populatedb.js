9e92d42222d7886018fab245f98e59a098c6768a
Velmoragroup <velmoraholdingsgroup@gmail.com>
2026-09-19 18:28:19 +0300

    issues with aiven

--- populatedb.js
+++ populatedb.js
@@ -513,21 +513,35 @@ function populatedb (){
         
         
         
+    // var mysql = require('mysql2');
+
+    // var con = mysql.createConnection({
+    //     host: process.env.DB_HOST || "localhost",
+    //     port: process.env.DB_PORT || 28678,
+    //     user: process.env.DB_USER || "root",
+    //     password: process.env.DB_PASSWORD || "password",
+    //     database: process.env.DB_NAME || 'easyClicksDatabase',
+    //     ssl: process.env.DB_HOST ? { rejectUnauthorized: false } : false,
+    //     waitForConnections: true,
+    //     connectionLimit: 10,
+    //     queueLimit: 0
+    // });
+    
+
+
     var mysql = require('mysql2');
 
     var con = mysql.createConnection({
         host: process.env.DB_HOST || "localhost",
-        port: process.env.DB_PORT || 28678,
+        port: process.env.DB_PORT || 3306,
         user: process.env.DB_USER || "root",
         password: process.env.DB_PASSWORD || "password",
         database: process.env.DB_NAME || 'easyClicksDatabase',
-        ssl: process.env.DB_HOST ? { rejectUnauthorized: false } : false,
+        ssl: { rejectUnauthorized: false }, // Always enforce SSL for cloud-hosted MySQL
         waitForConnections: true,
         connectionLimit: 10,
         queueLimit: 0
     });
-
-
 
 
 
