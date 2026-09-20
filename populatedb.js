2164fedc1db1045fece7fc244d751d359971fd4b
Velmoragroup <velmoraholdingsgroup@gmail.com>
2026-09-20 08:04:40 +0300

    AGAIN consoling aiven credentials

--- populatedb.js
+++ populatedb.js
@@ -524,11 +524,11 @@ function populatedb (){
 const mysql = require('mysql2'); // or 'mysql'
 
 console.log(  
-    `host: process.env.DB_HOST,
-  port:${ process.env.DB_PORT || 28678},
+    `host:${ process.env.DB_HOST},
+  port:${ process.env.DB_PORT},
   user: ${process.env.DB_USER},
   password: ${process.env.DB_PASSWORD},
-  database: ${process.env.DB_NAME || 'defaultdb'},
+  database: ${process.env.DB_NAME},
   ssl: {
     rejectUnauthorized: false} // Required for Aiven cloud SSL connections
  }
